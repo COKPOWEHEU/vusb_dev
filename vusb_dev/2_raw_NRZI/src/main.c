1fcc4ae8f7ae0c7e0f058000110476e190eaeddc
COKPOWEHEU <31559186+COKPOWEHEU@users.noreply.github.com>
2019-07-19 15:35:42 +0300

    Add files via upload

---
+++ vusb_dev/2_raw_NRZI/src/main.c
@@ -0,0 +1,37 @@
+#include <avr/io.h>
+#include <avr/interrupt.h>
+#include <util/delay.h>
+#include "usbconfig.h"
+#include "uart.h"
+
+void strhex(uint8_t x, char *str){
+  uint8_t lh = x & 0x0F;
+  x >>= 4;
+  if(x > 9)str[0]=(x-0x0A+'A'); else str[0]=(x+'0');
+  if(lh> 9)str[1]=(lh-0x0A+'A');else str[1]=(lh+'0');
+}
+
+void usbInit(void);
+
+int main(){
+  RLED_CONFIG(); RLED_PORT &=~ RLED_MASK;
+  GLED_CONFIG(); GLED_PORT &=~ GLED_MASK;
+  
+  uart_init(1152);
+  
+  usbInit();
+  usbDeviceDisconnect();
+  
+  uart_puts("Start\r\n");
+  _delay_ms(200);
+  
+  usbDeviceConnect();
+  sei();
+  while(1){
+  }
+}
+
+void usbInit(void){
+    USB_INTR_CFG |= USB_INTR_CFG_SET;
+    USB_INTR_ENABLE |= (1 << USB_INTR_ENABLE_BIT);
+}
---
+++ vusb_dev/2_raw_NRZI/src/usbconfig.h
@@ -0,0 +1,54 @@
+#ifndef __usbconfig_h_included__
+#define __usbconfig_h_included__
+
+#define USB_CFG_IOPORTNAME      D
+#define USB_CFG_DMINUS_BIT      3
+#define USB_CFG_DPLUS_BIT       2
+
+#define USB_CFG_PULLUP_IOPORTNAME   D
+#define USB_CFG_PULLUP_BIT          4
+
+#define RLED_PORT PORTD
+#define RLED_BIT 6
+#define RLED_CONFIG() DDRD |= (1<<(RLED_BIT))
+#define RLED_MASK (1<<(RLED_BIT))
+
+#define GLED_PORT PORTD
+#define GLED_BIT 7
+#define GLED_CONFIG() DDRD |= (1<<(GLED_BIT))
+#define GLED_MASK (1<<(GLED_BIT))
+
+#define BTN_CONFIG() do{ DDRD &=~(1<<5); PORTD |= (1<<5); }while(0)
+#define BTN_STATE ((PIND & (1<<5))!=0)
+
+#ifdef USB_CFG_PULLUP_IOPORTNAME
+  #define usbDeviceConnect()      ((USB_PULLUP_DDR |= (1<<USB_CFG_PULLUP_BIT)), \
+                                  (USB_PULLUP_OUT |= (1<<USB_CFG_PULLUP_BIT)),\
+                                  (USBDDR &= ~(1<<USB_CFG_DMINUS_BIT)))
+  #define usbDeviceDisconnect()   ((USB_PULLUP_DDR &= ~(1<<USB_CFG_PULLUP_BIT)), \
+                                  (USB_PULLUP_OUT &= ~(1<<USB_CFG_PULLUP_BIT)),\
+                                  (USBDDR |= (1<<USB_CFG_DMINUS_BIT)))
+#else
+  #define usbDeviceConnect()      (USBDDR &= ~(1<<USB_CFG_DMINUS_BIT))
+  #define usbDeviceDisconnect()   (USBDDR |= (1<<USB_CFG_DMINUS_BIT))
+#endif
+
+
+#define USB_INTR_CFG MCUCR
+#define USB_INTR_ENABLE  GIMSK
+#define USB_INTR_CFG_SET (1 << ISC01) /*Int0 on falling edge*/
+#define USB_INTR_ENABLE_BIT  INT0
+
+
+#define USBOUT          PORTD
+#define USB_PULLUP_OUT  PORTD
+#define USBIN           PIND
+#define USBDDR          DDRD
+#define USB_PULLUP_DDR  DDRD
+
+#define USBMINUS    USB_CFG_DMINUS_BIT
+#define USBPLUS     USB_CFG_DPLUS_BIT
+#define USBIDLE     (1<<USB_CFG_DMINUS_BIT) /* value representing J state */
+#define USBMASK     ((1<<USB_CFG_DPLUS_BIT) | (1<<USB_CFG_DMINUS_BIT))  /* mask for USB I/O bits */
+
+#endif
