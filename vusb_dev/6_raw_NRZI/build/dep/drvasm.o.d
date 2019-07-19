886f6d070d829575b2754f9ae807822bcb5f2aed
COKPOWEHEU <31559186+COKPOWEHEU@users.noreply.github.com>
2019-07-19 15:37:47 +0300

    Add files via upload

---
+++ vusb_dev/6_raw_NRZI/build/dep/drvasm.o.d
@@ -0,0 +1,23 @@
+drvasm.o: src/drvasm.S /usr/lib/avr/include/avr/io.h \
+ /usr/lib/avr/include/avr/sfr_defs.h /usr/lib/avr/include/avr/iom8.h \
+ /usr/lib/avr/include/avr/portpins.h /usr/lib/avr/include/avr/common.h \
+ /usr/lib/avr/include/avr/version.h /usr/lib/avr/include/avr/fuse.h \
+ /usr/lib/avr/include/avr/lock.h src/usbconfig.h
+
+/usr/lib/avr/include/avr/io.h:
+
+/usr/lib/avr/include/avr/sfr_defs.h:
+
+/usr/lib/avr/include/avr/iom8.h:
+
+/usr/lib/avr/include/avr/portpins.h:
+
+/usr/lib/avr/include/avr/common.h:
+
+/usr/lib/avr/include/avr/version.h:
+
+/usr/lib/avr/include/avr/fuse.h:
+
+/usr/lib/avr/include/avr/lock.h:
+
+src/usbconfig.h:
---
+++ vusb_dev/6_raw_NRZI/build/dep/main.o.d
@@ -0,0 +1,43 @@
+main.o: src/main.c /usr/lib/avr/include/avr/io.h \
+ /usr/lib/avr/include/avr/sfr_defs.h /usr/lib/avr/include/inttypes.h \
+ /usr/lib/gcc/avr/5.4.0/include/stdint.h /usr/lib/avr/include/stdint.h \
+ /usr/lib/avr/include/avr/iom8.h /usr/lib/avr/include/avr/portpins.h \
+ /usr/lib/avr/include/avr/common.h /usr/lib/avr/include/avr/version.h \
+ /usr/lib/avr/include/avr/fuse.h /usr/lib/avr/include/avr/lock.h \
+ /usr/lib/avr/include/avr/interrupt.h /usr/lib/avr/include/util/delay.h \
+ /usr/lib/avr/include/util/delay_basic.h /usr/lib/avr/include/math.h \
+ src/usbconfig.h src/uart.h
+
+/usr/lib/avr/include/avr/io.h:
+
+/usr/lib/avr/include/avr/sfr_defs.h:
+
+/usr/lib/avr/include/inttypes.h:
+
+/usr/lib/gcc/avr/5.4.0/include/stdint.h:
+
+/usr/lib/avr/include/stdint.h:
+
+/usr/lib/avr/include/avr/iom8.h:
+
+/usr/lib/avr/include/avr/portpins.h:
+
+/usr/lib/avr/include/avr/common.h:
+
+/usr/lib/avr/include/avr/version.h:
+
+/usr/lib/avr/include/avr/fuse.h:
+
+/usr/lib/avr/include/avr/lock.h:
+
+/usr/lib/avr/include/avr/interrupt.h:
+
+/usr/lib/avr/include/util/delay.h:
+
+/usr/lib/avr/include/util/delay_basic.h:
+
+/usr/lib/avr/include/math.h:
+
+src/usbconfig.h:
+
+src/uart.h:
